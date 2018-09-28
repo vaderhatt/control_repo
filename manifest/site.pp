43c7e44fb4af129238d347a0b52c5b964e2d34e6
vaderhatt <43676357+vaderhatt@users.noreply.github.com>
2018-09-28 17:37:02 +0300

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,7 +1,7 @@
 node default {
-  file {'/root/readme':
+  file { '/root/README':
     ensure => file,
-    content => 'this is a readme',
-    owner => 'root',
+    content => 'This is a readme',
+    owner   => 'root',
   }
 }
